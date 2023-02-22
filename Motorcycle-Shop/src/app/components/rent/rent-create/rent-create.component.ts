534310f4f73771441a4fb528411fda5a1c8ec42c
Michael <52010116@edu.campus02.at>
2023-02-23 00:28:49 +0100

    small adjustments for special request pt 2

--- Motorcycle-Shop/src/app/components/rent/rent-create/rent-create.component.ts
+++ Motorcycle-Shop/src/app/components/rent/rent-create/rent-create.component.ts
@@ -9,7 +9,10 @@ import { RentService } from '../rent.service';
   styleUrls: ['./rent-create.component.css']
 })
 export class RentCreateComponent implements OnInit{
-  motorcycle: Motorcycle = {
+  showSuccessMessage = false;
+  success: boolean = false;
+
+  request: Motorcycle = {
     id: 100,
     brand: 'Yamaha',
     model: 'Tracer',
@@ -18,28 +21,50 @@ export class RentCreateComponent implements OnInit{
     ccm: 660,
     kilometers: 14987
   };
+  submitted = false;
 
   constructor(private rentService: RentService) { }
 
   ngOnInit(): void {
   }
 
-  // in der var "data" wird der Post gespeichert
+  // variable data saves new motorcycle entry
   createMotorcycle(): void {
     const data = {
-      id: this.motorcycle.id,
-      brand: this.motorcycle.brand,
-      model: this.motorcycle.model,
-      year: this.motorcycle.year,
-      hp: this.motorcycle.hp,
-      ccm: this.motorcycle.ccm,
-      kilometers: this.motorcycle.kilometers
+      id: this.request.id,
+      brand: this.request.brand,
+      model: this.request.model,
+      year: this.request.year,
+      hp: this.request.hp,
+      ccm: this.request.ccm,
+      kilometers: this.request.kilometers
     };
 
 
     this.rentService.createMotorcycle(data)
       .subscribe(() => {
         console.log('Motorcycle created successfully');
+        this.showSuccessMessage = true;
+        this.success = true;
+        this.submitted = true;
+        setTimeout(() => {
+          this.showSuccessMessage = false;
+        }, 3000);
       });
+  }
+
+  // generate a new request
+  newRequest(): void {
+    this.success = false;
+    this.submitted = false;
+    this.request = {
+    id: 0,
+    brand: 'Yamaha',
+    model: 'MT',
+    year: 2023,
+    hp: 75,
+    ccm: 700,
+    kilometers: 0
+    };
   }
 }
