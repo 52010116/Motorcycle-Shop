722dc8a52408ff9bfddaa788760aa3f545370b54
Michael <52010116@edu.campus02.at>
2023-02-23 16:07:50 +0100

    changed folder struture and routes

--- Motorcycle-Shop/src/app/components/rent/rent.routes.ts
+++ Motorcycle-Shop/src/app/components/rent/rent.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from '@angular/router';
 
 import { RentSearchComponent } from './rent-search/rent-search.component';
 import { RentCreateComponent } from './rent-create/rent-create.component';
-import { RentUpdateComponent } from './rent-update/rent-update.component';
-import { RentDeleteComponent } from './rent-delete/rent-delete.component';
+
 
 
 export const RENT_ROUTES: Routes = [
@@ -14,13 +13,5 @@ export const RENT_ROUTES: Routes = [
   {
     path: "request",
     component: RentCreateComponent
-  },
-  {
-    path: "delete",
-    component: RentDeleteComponent
-  },
-  {
-    path: "edit",
-    component: RentUpdateComponent
   },
 ];
